6001d35bcbbf3fcf35f3a791866ba9335badb6e4
ShelemetevP <spp55lis@gmail.com>
2021-11-02 05:17:56 +0600

    CPC5

--- app/src/main/java/com/training/spp_app/MainActivity.kt
+++ app/src/main/java/com/training/spp_app/MainActivity.kt
@@ -1,34 +1,69 @@
 package com.training.spp_app
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.contentValuesOf
+import androidx.core.util.rangeTo
 
-class MainActivity() : AppCompatActivity(), Parcelable {
-    constructor(parcel: Parcel) : this() {
-    }
+private const val TAG = "MY_LOG"
+private const val VALUE = "Value"
+
+class MainActivity() : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "На языке, тебе невнятном")
+        Log . d (TAG, "Стихи прощальные пишу")
     }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Но в заблуждении приятном")
+        Log.d(TAG, "Вниманья твоего прошу:")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Мой друг, доколе не увяну,")
+        Log.d(TAG, "В разлуке чувство погубя,")
 
     }
 
-    override fun describeContents(): Int {
-        return 0
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Боготворить не перестану")
+        Log.d(TAG, "Тебя, мой друг, одну тебя.")
     }
 
-    companion object CREATOR : Parcelable.Creator<MainActivity> {
-        override fun createFromParcel(parcel: Parcel): MainActivity {
-            return MainActivity(parcel)
-        }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "На чуждые черты взирая,")
+        Log.d(TAG, "Верь только сердцу моему,")
+    }
 
-        override fun newArray(size: Int): Array<MainActivity?> {
-            return arrayOfNulls(size)
-        }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Как прежде верила ему,")
+        Log.d(TAG, "Его страстей не понимая.")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 }
+
+
+
